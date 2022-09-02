b497b0f6a7d1b18004f28063213a1ec342dac216
Waldemar Kołodziejczyk <waldemar.kolodziejczyk@airev.net>
2022-09-02 17:01:47 +0200

    Added model downloading

--- prediction.py
+++ prediction.py
@@ -1,11 +1,23 @@
+from pathlib import Path
 from typing import Dict
 
 import joblib
 
 from preprocessor import TextCleaner
+import gdown
+
+
+def load_pipeline(model_path: Path):
+    if not model_path.exists():
+        gdown.download('https://drive.google.com/uc?export=download&id=1IjK8lMhIo2iqd85D7qo93plNXmliIAsU',
+                       str(model_path),
+                       quiet=False)
+    return joblib.load(model_path)
+
 
 text_cleaner = TextCleaner()
-pipeline = joblib.load('classifier_pipeline.pkl')
+pipeline = load_pipeline(Path('classifier_pipeline.pkl'))
+
 
 def predict(body: Dict):
     review_text = body.get('review', None)
